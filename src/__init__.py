3f7ca3dc63eb7f1dcf234bc17a39719dc6953042
mehaotian <490272692@qq.com>
2024-07-16 11:45:30 +0800

    fix: 修改build命令，增加拉取最新代码的操作

--- src/__init__.py
+++ src/__init__.py
@@ -12,6 +12,6 @@ async def ignore_prod(event: Event):
     config = nonebot.get_driver().config
     openid = str(event.group_openid)
     group_openid = config.groups_openid
-    print(group_openid)
     if group_openid and openid in group_openid :
+        print('已经忽略群：', openid)
         raise IgnoredException("信息已经忽略")
